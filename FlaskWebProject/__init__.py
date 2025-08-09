9492716575acd5ed370a38114386f98f48c281bb
Federico Perchuk <federico.perchuk@chevron.com>
2025-08-09 02:02:34 -0300

    Add logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,7 +12,12 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
-# TODO: Add any logging levels and handlers with app.logger
+
+app.logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
