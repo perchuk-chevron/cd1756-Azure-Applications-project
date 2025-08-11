87443d79afcf08e982f61d48c77866e796680417
Federico Perchuk <federico.perchuk@chevron.com>
2025-08-11 18:44:46 -0300

    Fix logger

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,7 +66,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.warnig(f"Failed login attempt for user: {user.username}")
+            app.logger.warning(f"Failed login attempt for user: {user.username}")
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
